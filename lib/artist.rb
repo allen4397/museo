2bf2f06c1032de9158b1c3676b45a8f408ac1cd3
Tim Allen <tnelsallen3@gmail.com>
2019-01-02 13:19:39 -0700

    Adds artist attributes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,15 @@
 class Artist
+  attr_reader :id,
+              :name,
+              :born,
+              :died,
+              :country
+
   def initialize(attributes)
-    @attributes = attributes
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
   end
 end
